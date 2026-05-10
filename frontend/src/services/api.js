0abee5bad8da662c9cae4ba9f60699b4d098290c
tanmaylohakare-bridgelabz <tanmaylohakare@bridgelabz.com>
2026-05-10 19:54:13 +0530

    [Tanmay] Modified service in Frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/quantity';
+const url = import.meta.env.VITE_API_URL;
+
+const API_BASE_URL = `${url}/api/quantity`;
+// console.log(API_BASE_URL);
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('jwt_token');
